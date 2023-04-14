3f86d348f67577b70111dddfcb07ca7a530a8132
Frederic Herzog <127777563+Frederic2302@users.noreply.github.com>
2023-04-14 11:19:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function() { // die Funktion bekommt eine Adresse und löst die aus
+    console.log("Ich werde ausgeführt, wenn der DOM geladen ist.")
+}); // mit DOMContent Loaded können wir die Race Condidtion unterbrechen 
